7ca2985fece77b83c7dc294804646cb7b3a0cc4f
Miguel Lucio Silva <miguelluciosilva@gmail.com>
2023-08-07 12:55:34 -0300

    estilizando modal

--- src/components/CartModal/CartItemCard/index.jsx
+++ src/components/CartModal/CartItemCard/index.jsx
@@ -1,19 +1,24 @@
 import { MdDelete } from "react-icons/md";
+import styles from "./style.module.scss";
 
 export const CartItemCard = ({ product, removeCartList }) => {
   return (
-    <li>
-      <span>{product.count}</span>
-      <div>
-        <img src={product.img} alt={product.name} />
-        <h3>{product.name}</h3>
+    <li className={styles.cardItem}>
+      <div className={styles.productBox}>
+        <span>
+          <p>{product.count}</p>
+        </span>
+        <div>
+          <img src={product.img} alt={product.name} />
+        </div>
+        <h3 className="title three gray100">{product.name}</h3>
       </div>
       <button
         aria-label="delete"
         title="Remover item"
         onClick={() => removeCartList(product.id)}
       >
-        <MdDelete size={21} />
+        <MdDelete size={20} color="#bdbdbd" />
       </button>
     </li>
   );
--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -2,6 +2,7 @@ import { MdClose } from "react-icons/md";
 import { CartItemCard } from "./CartItemCard";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import styles from "./style.module.scss";
 
 export const CartModal = ({
   cartList,
@@ -19,39 +20,51 @@ export const CartModal = ({
   };
 
   return (
-    <div role="dialog">
-      <div>
-        <h2>Carrinho de compras</h2>
-        <button
-          aria-label="close"
-          title="Fechar"
-          onClick={() => setIsVisible(false)}
-        >
-          <MdClose size={21} />
-        </button>
-      </div>
-      <div>
-        <ul>
-          {cartList.map((product) => (
-            <CartItemCard
-              key={product.id}
-              removeCartList={removeCartList}
-              product={product}
-            />
-          ))}
-        </ul>
-      </div>
-      <div>
-        <div>
-          <span>Total</span>
-          <span>
-            {total.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            })}
-          </span>
+    <div className={styles.overlayBox} role="dialog">
+      <div className={styles.modalBox}>
+        <div className={styles.headerModal}>
+          <h2 className="title three white">Carrinho de compras</h2>
+          <button
+            aria-label="close"
+            title="Fechar"
+            onClick={() => setIsVisible(false)}
+          >
+            <MdClose size={21} color="#ffffff" />
+          </button>
         </div>
-        <button onClick={clearCardList}>Remover todos</button>
+        <div className={styles.bodyModal}>
+          <div className={styles.listBox}>
+            {cartList.length > 0 ? (
+              <ul>
+                {cartList.map((product) => (
+                  <CartItemCard
+                    key={product.id}
+                    removeCartList={removeCartList}
+                    product={product}
+                  />
+                ))}
+              </ul>
+            ) : (
+              <p className="title three gray100">
+                Opss... O Carrinho está vazio!
+              </p>
+            )}
+          </div>
+          <div className={styles.amountBox}>
+            <div>
+              <span className="text body600 gray100">Total</span>
+              <span className="text body600 gray200">
+                {total.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </span>
+            </div>
+            <button className="btn large" onClick={clearCardList}>
+              Remover todos
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
